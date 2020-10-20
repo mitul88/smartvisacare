dfb8f3049b52db0d9788b2f9604e8ff5cc7775a9
shahed <shahed.rahman88@gmail.com>
2020-10-20 22:15:55 +0600

    add jQuery smoothScroll, remove aboutSection, add mediaQuery

--- js/main.js
+++ js/main.js
@@ -13,3 +13,19 @@ window.addEventListener("scroll", () => {
   const navBar = document.querySelector("nav");
   navBar.classList.toggle("nav-change", window.scrollY > 0);
 });
+
+
+// jQuery smoothScroll
+$('.navbar a').on('click',function(e){
+  if('this.hash' !== ''){
+    e.preventDefault();
+
+    const hash = this.hash;
+
+    $('html, body').animate({
+      scrollTop: $(hash).offset().top
+    },
+    800
+    );
+  }
+});
